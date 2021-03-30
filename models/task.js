1323c08cde167133a69f3cb4ef333b3aacdd35f3
LauraRomina2609 <79378669+LauraRomina2609@users.noreply.github.com>
2021-03-29 21:07:57 -0300

    `

---
+++ models/task.js
@@ -0,0 +1,18 @@
+const { v4: uuidv4 } = require('uuid');
+
+class Task {
+    id = ``;
+    title = ``;
+    done = null;
+    created = null;
+    finished = null;
+
+    constructor(title) {
+        this.id = uuidv4(); //TODO: asignar id
+        this.title = title;
+        this.done = false;
+        this.created = new Date(); //fecha de el momento actuao
+    }
+}
+
+module.exports = Task;
